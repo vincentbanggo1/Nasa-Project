d17613d50ccd148608bd9fd2254394036eeb465e
unknown <vincentbanggo1@gmail.com>
2026-06-05 18:58:29 +0800

    Initial commit

---
+++ server/src/models/planets.models.js
@@ -0,0 +1,39 @@
+const { parse } = require('csv-parse');
+const path = require('path');
+const fs = require('fs');
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+function loadPlanetsData() {
+    return new Promise ((resolve, rejects) => {
+     fs.createReadStream(path.join(__dirname,'..','..','data','kepler_data (2).csv'))
+  .pipe(parse({
+    comment: '#',
+    columns: true,
+  }))
+  .on('data', (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on('error', (err) => {
+    console.log(err);
+    rejects(err);
+  })
+  .on('end', () => {
+    console.log(`${habitablePlanets.length} habitable planets found!`);
+    resolve();
+  });
+});
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
