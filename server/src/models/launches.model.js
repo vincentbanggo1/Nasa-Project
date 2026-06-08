9d23a82c6471db77c1278639ae58d30d5510a0ce
unknown <vincentbanggo1@gmail.com>
2026-06-08 14:51:11 +0800

    Normalize line endings

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -16,6 +16,10 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
+function existLaunchWithId(launchId){
+  return launches.has(launchId);
+}
+
 function getAllLaunches() {
  return Array.from(launches.values());
 }
@@ -32,8 +36,17 @@ function addNewLaunch(launch){
   }));
 }
 
+function abortLaunchById(launchId){
+ const aborted = launches.get(launchId);
+ aborted.upcoming = false;
+ aborted.success = false;
+ return aborted;
+}
+
 module.exports = {
+existLaunchWithId,
 getAllLaunches,
 addNewLaunch,
+abortLaunchById,
 
 };
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,6 +1,8 @@
 const {
      getAllLaunches,
      addNewLaunch ,
+     existLaunchWithId,
+     abortLaunchById,
 } = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
@@ -21,7 +23,7 @@ if (!launch.mission || !launch.rocket || !launch.launchDate
 launch.launchDate = new Date(launch.launchDate);
 if (isNaN(launch.launchDate)) {
     return res.status(400).json({
-        error: 'Invalid lauch date',
+        error: 'Invalid launch date',
     });
 }
 
@@ -29,7 +31,19 @@ addNewLaunch(launch);
 return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req,res){
+const launchId = Number(req.params.id);
+if (!existLaunchWithId(launchId)){
+  return res.status(404).json({
+  error: 'Launch not found'
+});  
+}
+const aborted = abortLaunchById(launchId)
+return res.status(200).json(aborted);
+}
+
 module.exports = {
     httpGetAllLaunches,
     httpAddNewLaunch,
-}
+    httpAbortLaunch,
+};
